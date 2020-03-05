36878641939c79dafb47d77a6caf5d87da5f7eb1
Zi Ye <18612319597@163.com>
2020-03-05 22:04:24 +0800

    pass the value through the route

--- lib/main.dart
+++ lib/main.dart
@@ -61,6 +61,20 @@ class _MyHomePageState extends State<MyHomePage> {
                     })
                 );
               },
+            ),
+            FlatButton(
+              child: Text('pass the value through the route'),
+              textColor: Colors.red,
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return RouterTestRoute();
+                    }
+                  )
+                );
+              },
             )
           ],
         ),
@@ -84,6 +98,59 @@ class NewRoute extends StatelessWidget {
       body: Center(
         child: Text('This is new route')
       )
+    );
+  }
+}
+
+class TipRoute extends StatelessWidget {
+  TipRoute({Key key, @required this.text}) : super(key: key);
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('提示')
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(18),
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Text(text),
+              RaisedButton(
+                onPressed: () => Navigator.pop(context, '我是返回值'),
+                child: Text('返回')
+              )
+            ],
+          )
+        )
+      )
+    );
+  }
+}
+
+class RouterTestRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: RaisedButton(
+        onPressed: () async {
+          var result = await Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) {
+                return TipRoute(
+                    text: '我是提示xxx'
+                );
+              }
+            )
+          );
+          print('路由返回值: ${result}');
+        },
+        child: Text('打开提示页')
+      ),
     );
   }
 }
